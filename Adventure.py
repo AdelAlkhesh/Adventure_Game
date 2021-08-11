d64df8ecf646d53fdb5d5776a4eac1238b608eed
Shen <splinteredsanity@outlook.com>
2021-08-11 13:44:31 +0200

    Initial Outline and welcome screen.

---
+++ Adventure.py
@@ -0,0 +1,10 @@
+place_holder = "PlaceHolder"
+
+print(place_holder)
+print("Hello and welcome to", place_holder + "!" + "\nThis is a text-based choose your own adeventure game where the choices you make affect the outcome and progression of the game.")
+print("You navigate this game by typing out your choices in the terminal when prompted to do so.")
+print("Choices will consist of: \n-Yes or no questions (Y/n) \n-Exploration options \n-Item pickups")
+print("Try to take your time and explore the places you find yourself in. You never know what you might find!")
+print("That's it for the tutorial. Good luck, and have fun!")
+
+
